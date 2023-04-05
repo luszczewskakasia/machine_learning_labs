c37bfc574feb26988d5ed50eba83225853d13b68
luszczewskakasia <kasia.luszczewska13@gmail.com>
2023-04-05 22:29:37 +0200

    projekt

---
+++ svm_spam__skeleton/get_vocabulary_dict.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import csv
+
+from typing import Dict
+
+
+def get_vocabulary_dict() -> Dict[int, str]:
+    """Read the fixed vocabulary list from the datafile and return.
+
+    :return: a dictionary of words mapped to their indexes
+    """
+
+    # FIXME: Parse data from the 'data/vocab.txt' file.
+    # - The file is saved in tab-separated values (TSV) format.
+    # - Each line contains a word's ID and the word itself.
+    # The output dictionary should map word's ID on the word itself, e.g.:
+    #   {1: 'aa', 2: 'ab', ...}
+    diction = {}
+    with open('vocab.txt', 'r') as file:
+        words = file.read()
+        for i in words:
+            diction[(i[0])] = i[1]
+
+    file.close()
+    return diction
+
+print(get_vocabulary_dict())
